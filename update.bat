48f2cfbd5ab4cfab8d0f219115f64e99615001b8
Yuki Kurosawa Hotkey <yuki@ksyuki.com>
2025-12-17 16:33:41 +0800

    update scripts

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 docker build -f DAK.Dockerfile -t yukikurosawadev/debian_archive_kit:test-build .
-docker run -it --rm --privileged yukikurosawadev/debian_archive_kit:test-build
+docker run -it --rm --privileged -p 5432:5432 -p 80:80 yukikurosawadev/debian_archive_kit:test-build
 docker rmi yukikurosawadev/debian_archive_kit:test-build
